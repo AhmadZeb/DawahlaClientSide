b263f379523642fbd2b6771d47ebcdc643dff1e2
Ahmad Zeb <ahmadzebmcs@outlook.com>
2021-10-07 14:34:53 -0700

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OrderComponentComponent } from './components/orderComponent/order-component/order-component.component';
 import { UserComponentComponent } from './components/userComponent/user-component/user-component.component';
+import { ProductComponentComponent } from './components/productComponent/product-component/product-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OrderComponentComponent,
-    UserComponentComponent
+    UserComponentComponent,
+    ProductComponentComponent
   ],
   imports: [
     BrowserModule,
