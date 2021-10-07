7e46f4a10f5b52319aa96ecc72f6e72238ee8b37
Ahmad Zeb <ahmadzebmcs@outlook.com>
2021-10-07 13:08:59 -0700

    add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OrderComponentComponent } from './components/orderComponent/order-component/order-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrderComponentComponent
   ],
   imports: [
     BrowserModule,
